d6f926016c49d07788b1e9a28b69fd9a581c0239
shubham <shubham@gmail.com>
2025-03-08 13:51:22 +0530

    adding Database

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.use('/api/likes', likeRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
-  
+  await sequelize.sync(); 
 });
